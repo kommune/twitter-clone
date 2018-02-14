ff6783ebea5debf67eac364d89b388e6e121c605
kommune <kommune.000@gmail.com>
2018-02-14 14:13:30 +0800

    add column image to tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214045420) do
+ActiveRecord::Schema.define(version: 20180214061045) do
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
     t.text "tweet", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
