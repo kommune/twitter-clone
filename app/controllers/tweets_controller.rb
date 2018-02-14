adf8b192dd69b7de07ae2afc226545ab5585ad3c
kommune <kommune.000@gmail.com>
2018-02-14 15:58:49 +0800

    debugged tweet routes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = current_user.tweets.new(tweet_params)
     if @tweet.save
       flash[:notice] = "Tweet was successfully created!"
       redirect_to tweets_path
