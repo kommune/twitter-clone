5c804719d5618fd4292149879c7c731cdacb65a1
kommune <kommune.000@gmail.com>
2018-02-14 13:09:10 +0800

    set up db and tables

---
+++ db/migrate/20180214045033_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tweets do |t|
+      t.belongs_to :user, foreign_key: true
+      t.text "tweet", null: false
+      t.timestamps
+    end
+  end
+end
