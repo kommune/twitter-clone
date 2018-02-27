17b76a3f2bf0a4a383a4af1c77b4f0daed438582
GLstark <Fredrick.tgl@gmail.com>
2018-02-27 15:51:18 +0800

    dependencies destroy

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,7 +1,7 @@
 class Hashtag < ApplicationRecord
 
   validates :hashtag, presence: true, uniqueness: {case_sensitive: false}
-  has_many :hashtagstweets
+  has_many :hashtagstweets, dependent: :destroy
   has_many :tweets, through: :hashtagstweets
 
 end
